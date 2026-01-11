923c26a38f207db276f02ac33b36959dc9796c77
N. Escobar <nick@nickesc.io>
2026-01-10 19:21:22 -0500

    added init subcommand to initialize new modpack.json file closes: #6

--- src/modpack-lock.js
+++ src/modpack-lock.js
@@ -1,10 +1,126 @@
 #!/usr/bin/env NODE_OPTIONS=--no-warnings node
 
 import { Command } from 'commander';
+import prompts from 'prompts';
+import path from 'path';
 import generateLockfile from './generate-lockfile.js';
+import generateJson from './generate-json.js';
 
 import pkg from '../package.json' with { type: 'json' };
 const modpackLock = new Command('modpack-lock');
+
+function validateNotEmpty(value, field) {
+  if (value.trim().length === 0) {
+    return `${field} cannot be empty`;
+  }
+  return true;
+}
+
+/**
+ * Get user input for modpack information
+ */
+async function getModpackInfo(defaultName) {
+    let answers = await prompts([
+        {
+          type: 'text',
+          name: 'name',
+          message: 'Modpack name',
+          initial: defaultName,
+          validate: (value) => {
+            return validateNotEmpty(value, 'Name');
+          },
+        },
+        {
+          type: 'text',
+          name: 'version',
+          message: 'Modpack version',
+          initial: '1.0.0',
+          validate: (value) => {
+            return validateNotEmpty(value, 'Version');
+          },
+        },
+
+        {
+          type: 'text',
+          name: 'id',
+          message: 'Modpack slug/ID',
+          initial: defaultName,
+          validate: (value) => {
+            return validateNotEmpty(value, 'ID');
+          },
+        },
+        {
+          type: 'text',
+          name: 'description',
+          message: 'Modpack description',
+        },
+        {
+          type: 'text',
+          name: 'author',
+          message: 'Modpack author',
+          validate: (value) => {
+            return validateNotEmpty(value, 'Author');
+          },
+        },
+        {
+          type: 'text',
+          name: 'projectUrl',
+          message: 'Modpack URL',
+        },
+        {
+          type: 'text',
+          name: 'sourceUrl',
+          message: 'Modpack source code URL',
+        },
+        {
+          type: 'text',
+          name: 'license',
+          message: 'Modpack license',
+          initial: 'MIT',
+        },
+        {
+          type: 'autocomplete',
+          name: 'modloader',
+          message: 'Modpack modloader',
+          choices: [
+            { title: 'fabric'},
+            { title: 'forge'},
+            { title: 'quilt'},
+            { title: 'neoforge'},
+            { title: 'sponge'},
+            { title: 'paper'},
+            { title: 'velocity'},
+            { title: 'bungeecord'},
+            { title: 'waterfall'},
+            { title: 'travertia'},
+            { title: 'nukkit'},
+            { title: 'pufferfish'},
+            { title: 'purpur'},
+          ],
+          validate: (value) => {
+            return validateNotEmpty(value, 'Modloader');
+          },
+        },
+        {
+          type: 'text',
+          name: 'targetModloaderVersion',
+          message: 'Target modloader version',
+        },
+        {
+          type: 'text',
+          name: 'targetMinecraftVersion',
+          message: 'Target Minecraft version',
+          validate: (value) => {
+            return validateNotEmpty(value, 'Minecraft Version');
+          },
+        }
+    ]);
+    if (Object.keys(answers).length === 0) {
+      console.warn('Modpack initialization was interrupted');
+      process.exit(1);
+    }
+    return answers;
+}
 
 modpackLock
   .name(pkg.name)
@@ -24,6 +140,21 @@ modpackLock
   .version(pkg.version)
   .action((options) => {
     generateLockfile({ ...options, path: options.path || process.cwd() }).catch(error => {
+      console.error('Error:', error);
+      process.exit(1);
+    });
+  });
+
+modpackLock.command('init')
+  .description('Initialize a new modpack')
+  .optionsGroup("Options:")
+  .option('-p, --path <path>', 'Path to the modpack directory')
+  .action((options) => {
+    console.log("This utility will walk you through creating a modpack.json file.\nIt only covers the most common items, and tries to guess sensible defaults.\n");
+    console.log("Press ^C at any time to quit.\n");
+    getModpackInfo(path.basename(options.path || process.cwd())).then(modpackInfo => {
+        generateJson(modpackInfo, [], [options.path || process.cwd()])
+    }, error => {
       console.error('Error:', error);
       process.exit(1);
     });
