25f02e67b402860eaf4a55d51cca5665787e0441
N. Escobar <nick@nickesc.io>
2026-01-14 00:38:25 -0500

    cleaned up prompting

--- src/modpack_info.js
+++ src/modpack_info.js
@@ -23,113 +23,118 @@ function validateNotEmpty(value, field) {
  * @returns {Promise<ModpackInfo>} The modpack information from the user
  */
 export async function promptUserForInfo(defaults = {}) {
-    let name = await prompts({
+    const licenseList = await getLicenseList();
+    let answers = await prompts([{
         type: 'text',
         name: 'name',
         message: 'Modpack name',
         initial: defaults.name,
         validate: (value) => {
             return validateNotEmpty(value, 'Name');
-        },
-    });
-    let version = await prompts({
+        }
+    },
+    {
         type: 'text',
         name: 'version',
         message: 'Modpack version',
         initial: defaults.version || config.DEFAULT_MODPACK_VERSION,
         validate: (value) => {
             return validateNotEmpty(value, 'Version');
-        },
-    });
-    let id = await prompts({
+        }
+    },
+    {
         type: 'text',
         name: 'id',
         message: 'Modpack slug/ID',
-        initial: slugify(defaults.id || name.name, config.SLUGIFY_OPTIONS),
+        initial: (prev, values) => slugify(defaults.id || values.name, config.SLUGIFY_OPTIONS),
         validate: (value) => {
             return validateNotEmpty(value, 'ID');
-        },
-    });
-    let description = await prompts({
+        }
+    },
+    {
         type: 'text',
         name: 'description',
         message: 'Modpack description',
         initial: defaults.description,
-    });
-    let author = await prompts({
+    },
+    {
         type: 'text',
         name: 'author',
         message: 'Modpack author',
         initial: defaults.author,
         validate: (value) => {
             return validateNotEmpty(value, 'Author');
-        },
-    });
-    let answers = await prompts([
-        {
-            type: 'text',
-            name: 'projectUrl',
-            message: 'Modpack URL',
-            initial: defaults.projectUrl || config.DEFAULT_PROJECT_URL(id.id),
-        },
-        {
-            type: 'text',
-            name: 'sourceUrl',
-            message: 'Modpack source code URL',
-            initial: defaults.sourceUrl || config.DEFAULT_SOURCE_URL(id.id, author.author),
-        },
-        {
-            type: 'text',
-            name: 'license',
-            message: 'Modpack license',
-            initial: defaults.license || config.DEFAULT_MODPACK_LICENSE,
-        },
-        {
-            type: 'autocomplete',
-            name: 'modloader',
-            message: 'Modpack modloader',
-            initial: defaults.modloader,
-            choices: [
-                { title: 'fabric' },
-                { title: 'forge' },
-                { title: 'quilt' },
-                { title: 'neoforge' },
-                { title: 'sponge' },
-                { title: 'paper' },
-                { title: 'velocity' },
-                { title: 'bungeecord' },
-                { title: 'waterfall' },
-                { title: 'travertia' },
-                { title: 'nukkit' },
-                { title: 'pufferfish' },
-                { title: 'purpur' },
-            ],
-            validate: (value) => {
-                return validateNotEmpty(value, 'Modloader');
-            },
-        },
-        {
-            type: 'text',
-            name: 'targetModloaderVersion',
-            message: 'Target modloader version',
-            initial: defaults.targetModloaderVersion,
-        },
-        {
-            type: 'text',
-            name: 'targetMinecraftVersion',
-            message: 'Target Minecraft version',
-            initial: defaults.targetMinecraftVersion,
-            validate: (value) => {
-                return validateNotEmpty(value, 'Minecraft Version');
-            },
         }
+    },
+    {
+        type: 'text',
+        name: 'projectUrl',
+        message: 'Modpack URL',
+        initial: (prev, values) => defaults.projectUrl || config.DEFAULT_PROJECT_URL(values.id),
+    },
+    {
+        type: 'text',
+        name: 'sourceUrl',
+        message: 'Modpack source code URL',
+        initial: (prev, values) => defaults.sourceUrl || config.DEFAULT_SOURCE_URL(values.id, values.author),
+    },
+    {
+        type: 'autocomplete',
+        name: 'license',
+        message: 'Modpack license',
+        limit: 20,
+        initial: defaults.license || config.DEFAULT_MODPACK_LICENSE,
+        choices: licenseList,
+    },
+    {
+        type: 'autocomplete',
+        name: 'modloader',
+        message: 'Modpack modloader',
+        initial: defaults.modloader,
+        choices: [
+            { title: 'fabric' },
+            { title: 'forge' },
+            { title: 'quilt' },
+            { title: 'neoforge' },
+            { title: 'sponge' },
+            { title: 'paper' },
+            { title: 'velocity' },
+            { title: 'bungeecord' },
+            { title: 'waterfall' },
+            { title: 'travertia' },
+            { title: 'nukkit' },
+            { title: 'pufferfish' },
+            { title: 'purpur' },
+        ],
+        validate: (value) => {
+            return validateNotEmpty(value, 'Modloader');
+        },
+    },
+    {
+        type: 'text',
+        name: 'targetModloaderVersion',
+        message: 'Target modloader version',
+        initial: defaults.targetModloaderVersion,
+    },
+    {
+        type: 'text',
+        name: 'targetMinecraftVersion',
+        message: 'Target Minecraft version',
+        initial: defaults.targetMinecraftVersion,
+        validate: (value) => {
+            return validateNotEmpty(value, 'Minecraft Version');
+        },
+    }
     ]);
 
-    let modpackInfo = {...name, ...version, ...id, ...description, ...author, ...answers};
+    let modpackInfo = { ...answers };
+
+    // TODO: this might not be right. find a better way to ensure the user did not interrupt the prompts. need to do that for any other prompts we use as well, including the license text prompt.
     if (Object.keys(modpackInfo).length < 11) {
         console.warn('Modpack initialization was interrupted');
         process.exit(1);
     }
+
     return modpackInfo;
 }
 
