12d88e9d911e619271473f479a1c356daa5dbc27
N. Escobar <nick@nickesc.io>
2026-01-17 01:47:02 -0500

    update description for gitignore option

--- src/config/strings.js
+++ src/config/strings.js
@@ -55,7 +55,7 @@ export const fileFields = {
     },
     addGitignore: {
         prompt: 'Update the .gitignore file',
-        option: 'Update the .gitignore file exceptions for files not hosted on Modrinth'
+        option: 'Update the .gitignore file, including exceptions for files not hosted on Modrinth'
     },
     addReadme: {
         prompt: 'Generate README.md files',
