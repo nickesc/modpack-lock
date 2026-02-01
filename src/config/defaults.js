b5efdeccf30cf1b097aae05b007c8686f08319bc
N. Escobar <nick@nickesc.io>
2026-01-31 22:59:01 -0500

    fixed duplicates in license options

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -1,6 +1,6 @@
 export const DEFAULT_MODPACK_VERSION = "1.0.0";
 
-export const DEFAULT_MODPACK_LICENSE = "MIT";
+export const DEFAULT_MODPACK_LICENSE = "mit";
 
 export const DEFAULT_PROJECT_URL = (id) => {
     return `https://modrinth.com/modpack/${id}`;
