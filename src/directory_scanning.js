f02fb643f480ab0823d043ff6ed58be5e7ef5360
N. Escobar <nick@nickesc.io>
2026-02-14 18:43:09 -0500

    added error cause

--- src/directory_scanning.js
+++ src/directory_scanning.js
@@ -92,7 +92,7 @@ async function getJsonFile(directoryPath, filename) {
         return JSON.parse(fileContent);
     } catch (error) {
         if (error.code !== "ENOENT") {
-            throw new Error(`Error: Could not read file ${jsonPath}: ${error.message}`);
+            throw new Error(`Error: Could not read file ${jsonPath}: ${error.message}`, {cause: error});
         } else {
             return null;
         }
