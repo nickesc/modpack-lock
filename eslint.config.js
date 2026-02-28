bd5ab307f73ea5068814ae4f2982387bcee16608
N. Escobar <nick@nickesc.io>
2026-02-28 15:19:19 -0500

    add eslint-plugin-jsdoc support refs #21

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from "@eslint/js";
+import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
 import {defineConfig} from "eslint/config";
 
@@ -8,5 +9,11 @@ export default defineConfig([
         plugins: {js},
         extends: ["js/recommended"],
         languageOptions: {globals: globals.node},
+        ignores: ["dist/**/*.js", "coverage/**/*.js", "node_modules/**/*.js", "docs/**/*"],
+    },
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        ...jsdoc.configs["flat/recommended"],
+        ignores: ["dist/**/*.js", "coverage/**/*.js", "node_modules/**/*.js", "docs/**/*"],
     },
 ]);
