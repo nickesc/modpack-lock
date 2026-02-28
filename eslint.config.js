03306d8dc85c9ac4f1e48e4c6003b087a09ba5b0
N. Escobar <nick@nickesc.io>
2026-02-28 15:38:55 -0500

    nevermind maybe?

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import js from "@eslint/js";
-import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
 import {defineConfig} from "eslint/config";
 
@@ -9,11 +8,5 @@ export default defineConfig([
         plugins: {js},
         extends: ["js/recommended"],
         languageOptions: {globals: globals.node},
-        ignores: ["dist/**/*.js", "coverage/**/*.js", "node_modules/**/*.js", "docs/**/*"],
-    },
-    {
-        files: ["**/*.{js,mjs,cjs}"],
-        ...jsdoc.configs["flat/recommended"],
-        ignores: ["dist/**/*.js", "coverage/**/*.js", "node_modules/**/*.js", "docs/**/*"],
     },
 ]);
