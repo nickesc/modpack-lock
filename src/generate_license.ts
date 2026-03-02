606300478b20743367decd462da89b1c1bce828b
N. Escobar <nick@nickesc.io>
2026-03-02 01:21:45 -0500

    add type support to generateLicense

--- src/generate_license.ts
+++ src/generate_license.ts
@@ -3,14 +3,9 @@ import path from "path";
 import {getLicenseText} from "./github_interactions.js";
 import * as config from "./config/index.js";
 import {logm} from "./logger.js";
+import type {ModpackInfo, Options, InitOptions, Lockfile} from "./types/index.js";
 
-/**
- * @typedef {import('./config/types.js').ModpackInfo} ModpackInfo
- * @typedef {import('./config/types.js').Options} Options
- * @typedef {import('./config/types.js').InitOptions} InitOptions
- */
-
-async function writeLicense(licenseText, outputPath) {
+async function writeLicense(licenseText: string, outputPath: string) {
     await fs.writeFile(path.join(outputPath, config.MODPACK_LICENSE_NAME), licenseText, "utf-8");
     logm.generated(config.MODPACK_LICENSE_NAME, path.join(outputPath, config.MODPACK_LICENSE_NAME));
 }
@@ -23,15 +18,24 @@ async function writeLicense(licenseText, outputPath) {
  * @param {string} licenseTextOverride - The license text to override the default license text with
  * @returns {Promise<string> | null} The license text or null if the license text could not be generated
  */
-export default async function generateLicense(modpackInfo, workingDir, options = {}, licenseTextOverride = null) {
+export default async function generateLicense(
+    modpackInfo: ModpackInfo,
+    workingDir: string,
+    options: Options | InitOptions = {},
+    licenseTextOverride: string | null = null,
+): Promise<string | null> {
     logm.quietFromOptions(options);
 
     try {
         const spdxId = modpackInfo.license;
 
         let licenseText = licenseTextOverride || (await getLicenseText(spdxId));
-        licenseText = licenseText.replace("[year]", new Date().getFullYear());
-        licenseText = licenseText.replace("{{year}}", new Date().getFullYear());
+        if (!licenseText) {
+            throw new Error(`License text could not be generated for: ${spdxId}`);
+        }
+
+        licenseText = licenseText.replace("[year]", String(new Date().getFullYear()));
+        licenseText = licenseText.replace("{{year}}", String(new Date().getFullYear()));
         licenseText = licenseText.replace("[fullname]", modpackInfo.author);
         licenseText = licenseText.replace("{{fullname}}", modpackInfo.author);
         licenseText = licenseText.replace("[organization]", modpackInfo.author);
