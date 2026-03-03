3f17b531c29342d9a79a48c3e327f277ca9567c3
N. Escobar <nick@nickesc.io>
2026-03-03 02:40:28 -0500

    rename optiondObject file to options

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 export type * from "./Lockfile.js";
 export type * from "./Jsonfile.js";
 export type * from "./ModpackInfo.js";
-export type * from "./optionsObjects.js";
+export type * from "./options.js";
 export type * from "./modrinthResponses.js";
 export type * from "./githubResponses.js";
 export type * from "./contentFiles.js";
