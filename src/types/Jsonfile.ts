102487eae81372c7d811abab3a4d70b3ae2151b2
N. Escobar <nick@nickesc.io>
2026-03-02 23:08:45 -0500

    make dependencies optional in jsonfile

--- src/types/Jsonfile.ts
+++ src/types/Jsonfile.ts
@@ -1,7 +1,7 @@
 import type {DependencyCategory, ModpackInfo} from "./index.js";
 
 export type Jsonfile = ModpackInfo & {
-    dependencies: Record<DependencyCategory, string[]>;
+    dependencies?: Record<DependencyCategory, string[]>;
     scripts?: {
         [key: string]: string;
     };
