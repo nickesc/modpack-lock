83e037e72b21ca8e0d526d11ef4300bf224c8be1
N. Escobar <nick@nickesc.io>
2026-03-06 00:12:15 -0500

    add jsonfile jsdoc

--- src/types/Jsonfile.ts
+++ src/types/Jsonfile.ts
@@ -1,5 +1,11 @@
 import type {DependencyCategory, ModpackInfo} from "./index.js";
 
+/**
+ * The `modpack.json` file shape
+ * @property dependencies - The dependencies of the modpack
+ * @property scripts - The scripts of the modpack
+ * @property [key: string] - Any other properties of the modpack
+ */
 export type Jsonfile = ModpackInfo & {
     dependencies?: Record<DependencyCategory, string[]>;
     scripts?: {
