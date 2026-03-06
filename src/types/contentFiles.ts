f6848c1f073fa0465efd0015f548813d5e83dea2
N. Escobar <nick@nickesc.io>
2026-03-06 00:06:38 -0500

    add content file jsdocs

--- src/types/contentFiles.ts
+++ src/types/contentFiles.ts
@@ -1,10 +1,22 @@
 import type {DependencyCategory} from "./index.js";
 
+/**
+ * A directory that contains modpack content (e.g. mods, resourcepacks, datapacks, shaderpacks)
+ * @property name - The name of the directory
+ * @property path - The path to the directory
+ */
 export type ContentDirectory = {
     name: DependencyCategory;
     path: string;
 };
 
+/**
+ * A binary content file in the modpack -- a file tracked by the modpack lockfile
+ * @property path - The path to the file
+ * @property fullPath - The full path to the file
+ * @property hash - The hash of the file
+ * @property category - The content category of the file (e.g. mods, resourcepacks, datapacks, shaderpacks)
+ */
 export type ContentFile = {
     path: string;
     fullPath: string;
