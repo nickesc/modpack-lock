c6b03196f8d0ae132df6718650bf785b1f7b5c31
N. Escobar <nick@nickesc.io>
2026-03-06 00:10:11 -0500

    add github response jsdocs

--- src/types/githubResponses.ts
+++ src/types/githubResponses.ts
@@ -1,8 +1,17 @@
+/**
+ * One of the license objects in a response from the GitHub API
+ * @property spdx_id - The SPDX ID of the license
+ * @property key - The key of the license
+ */
 export type LicenseResponseItem = {
     spdx_id: string;
     key: string;
 };
 
+/**
+ * The response from the GitHub API for a license text
+ * @property body - The text of the license
+ */
 export type LicenseTextResponse = {
     body: string;
 };
