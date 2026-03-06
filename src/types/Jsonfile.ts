946c1ffcf222b818782dbba04d0bd4f049867380
N. Escobar <nick@nickesc.io>
2026-03-06 00:20:49 -0500

    update jsonfile description

--- src/types/Jsonfile.ts
+++ src/types/Jsonfile.ts
@@ -1,7 +1,7 @@
 import type {DependencyCategory, ModpackInfo} from "./index.js";
 
 /**
- * The `modpack.json` file shape
+ * The modpack.json file's shape; contains the modpack information and dependencies
  * @property dependencies - The dependencies of the modpack
  * @property scripts - The scripts of the modpack
  * @property [key: string] - Any other properties of the modpack
