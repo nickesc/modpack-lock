4e684625bb0428e2ca6aaa05d686f9d515515ddf
N. Escobar <nick@nickesc.io>
2026-03-06 00:21:10 -0500

    update and add lockfile jsdocs

--- src/types/Lockfile.ts
+++ src/types/Lockfile.ts
@@ -1,20 +1,20 @@
 import type {ContentVersion} from "./index.js";
 
 /**
- * Contains information about the modpack dependencies and their versions.
- * @property version - The version of the modpack
+ * Lockfile shape; contains detailed information about the modpack dependencies and their versions.
+ * @property version - The version of the lockfile
  * @property generated - The date and time the lockfile was generated
  * @property total - The total number of files in the modpack
- * @property counts - The counts object
- * @property counts.mods - The mods count
- * @property counts.resourcepacks - The resourcepacks count
- * @property counts.datapacks - The datapacks count
- * @property counts.shaderpacks - The shaderpacks count
- * @property dependencies - The dependencies object
- * @property dependencies.mods - The mods array
- * @property dependencies.resourcepacks - The resourcepacks array
- * @property dependencies.datapacks - The datapacks array
- * @property dependencies.shaderpacks - The shaderpacks array
+ * @property counts - An object containing the number of files in each category
+ * @property counts.mods - The number of mods in the modpack
+ * @property counts.resourcepacks - The number of resourcepacks in the modpack
+ * @property counts.datapacks - The number of datapacks in the modpack
+ * @property counts.shaderpacks - The number of shaderpacks in the modpack
+ * @property dependencies - An object containing all dependency version information
+ * @property dependencies.mods - An array with version information for each mod
+ * @property dependencies.resourcepacks - An array with version information for each resourcepack
+ * @property dependencies.datapacks - An array with version information for each datapack
+ * @property dependencies.shaderpacks - An array with version information for each shaderpack
  */
 export type Lockfile = {
     version: string;
@@ -29,8 +29,20 @@ export type Lockfile = {
     };
 };
 
+/**
+ * The categories of dependencies in the lockfile
+ * @property mods - The mods folder
+ * @property resourcepacks - The resourcepacks folder
+ * @property datapacks - The datapacks folder
+ * @property shaderpacks - The shaderpacks folder
+ */
 export type DependencyCategory = keyof Lockfile["dependencies"];
 
+/**
+ * The shape of a dependency in the lockfile
+ * @property path - The path to the dependency file in the modpack directory
+ * @property version - The version of the dependency, or null if the dependency is not found on Modrinth
+ */
 export type LockfileDependency = {
     path: string;
     version: ContentVersion | null;
