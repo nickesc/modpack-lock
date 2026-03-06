920bd339273f0f33d9ed24bd48673dc4b7a6a6cc
N. Escobar <nick@nickesc.io>
2026-03-06 00:24:31 -0500

    update modpackinfo jsdoc

--- src/types/ModpackInfo.ts
+++ src/types/ModpackInfo.ts
@@ -1,16 +1,16 @@
 /**
  * Contains information about the modpack that is not dependent on the lockfile.
  * @property name - The name of the modpack (Required)
- * @property version - The version of the modpack (Required)
- * @property description - The description of the modpack
- * @property id - The slug/ID of the modpack (Required)
+ * @property version - The modpack version (Required)
+ * @property description - A description of the modpack
+ * @property id - The modpack's slug/ID (Required)
  * @property author - The author of the modpack (Required)
- * @property projectUrl - The project URL of the modpack
- * @property sourceUrl - The source code URL of the modpack
- * @property license - The license of the modpack
- * @property modloader - The modloader of the modpack (Required)
- * @property targetModloaderVersion - The target modloader version of the modpack
- * @property targetMinecraftVersion - The target Minecraft version of the modpack (Required)
+ * @property projectUrl - The project's homepage URL
+ * @property sourceUrl - The project's source code URL
+ * @property license - The modpack's license
+ * @property modloader - The modpack's modloader (Required)
+ * @property targetModloaderVersion - The target modloader version
+ * @property targetMinecraftVersion - The target Minecraft version (Required)
  */
 export type ModpackInfo = {
     name: string;
