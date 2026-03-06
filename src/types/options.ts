79bfaf757e5e876d989df9da2da2b1a2c2913dbf
N. Escobar <nick@nickesc.io>
2026-03-06 00:32:41 -0500

    updated options jsdocs

--- src/types/options.ts
+++ src/types/options.ts
@@ -1,8 +1,8 @@
 /**
  * Contains options for the generation of the modpack files.
- * @property dryRun - Whether to dry run the generation
+ * @property dryRun - Whether to run in dry-run mode (no files written)
  * @property quiet - Whether to quiet the console output
- * @property silent - Whether to silent the console output
+ * @property silent - Whether to silence the console output
  * @property gitignore - Whether to generate a .gitignore file
  * @property readme - Whether to generate README.md files
  * @property licenseFile - Whether to generate a license file
@@ -19,22 +19,22 @@ export interface Options {
 
 /**
  * Contains options for the initialization of the modpack files.
- * @property folder - The folder to generate the modpack files in
- * @property noninteractive - Whether to run the interactive mode
+ * @property folder - The modpack root directory
+ * @property noninteractive - Whether to initialize in non-interactive mode
  * @property addLicense - Whether to add the license file to the modpack
  * @property addGitignore - Whether to generate .gitignore rules
  * @property addReadme - Whether to generate README.md files
- * @property name - The name of the modpack
- * @property version - The version of the modpack
- * @property id - The slug/ID of the modpack
- * @property description - The description of the modpack
- * @property author - The author of the modpack
- * @property projectUrl - The modpack's project URL
- * @property sourceUrl - The modpack's source code URL
- * @property license - The modpack's license
- * @property modloader - The modpack's modloader
- * @property targetModloaderVersion - The target modloader version
- * @property targetMinecraftVersion - The target Minecraft version
+ * @property name - The option to set the modpack name
+ * @property version - The option to set the version
+ * @property id - The option to set the slug/ID
+ * @property description - The option to set the description
+ * @property author - The option to set the author
+ * @property projectUrl - The option to set the project URL
+ * @property sourceUrl - The option to set the source code URL
+ * @property license - The option to set the license
+ * @property modloader - The option to set the modloader
+ * @property targetModloaderVersion - The option to set the target modloader version
+ * @property targetMinecraftVersion - The option to set the target Minecraft version
  * @property _init - Internal boolean added to indicate options come from the `init` command.
  */
 export interface InitOptions extends Options {
@@ -59,8 +59,8 @@ export interface InitOptions extends Options {
 
 /**
  * Contains options for the running scripts defined in modpack.json.
- * @property folder - The folder to look for modpack.json in
- * @property debug - Whether to print debug information about the executed script
+ * @property folder - The modpack root directory
+ * @property debug - Whether to print debug information
  * @property _run - Internal boolean added to indicate options come from the `run` command.
  */
 export interface RunOptions extends Options {
